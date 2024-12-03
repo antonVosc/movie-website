7c03889432c99b6e880ea7475207f0a2627118d4
Anton <anton.vosc@gmail.com>
2024-12-03 23:41:58 +0000

    Started to work with API and Redux

---
+++ src/features/currentQuerySlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+    contries: '',
+    genreId: '',
+    order: 'YEAR',
+    type: '',
+    year: '',
+    page: 1,
+};
+
+export const currentQuerySlice = createSlice({
+    name: 'currentQuerySlice',
+    initialState,
+    reducers: {
+    },
+});
+
+export default counterSlice.reducer;
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
 import { CssBaseline } from '@mui/material';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 
+import { store } from './app/store.js';
 import App from './components/App.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <>
+    <Provider store={store}>
         <CssBaseline />
         <App />
-    </>,
+    </Provider>,
 );
